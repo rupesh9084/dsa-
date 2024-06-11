e6733acb171a92918db50e799cddeac486ffc410
Rupesh Gupta <142488982+rupesh9084@users.noreply.github.com>
2024-06-12 01:16:38 +0530

    Add files via upload

---
+++ desinding.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[50], i, j, temp, n;
+    printf("enter the size of array:");
+    scanf("%d", &n);
+    printf("enter the %d number ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("the sorted array is:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
