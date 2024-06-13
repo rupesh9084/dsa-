d52159161949c69642d52628cb25bb565bf9bdd1
Rupesh Gupta <142488982+rupesh9084@users.noreply.github.com>
2024-06-13 23:23:43 +0530

    Add files via upload

---
+++ insertion_sort_desending.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+int arr[50],i,j,temp,n;
+printf("enter the size of array:");
+scanf("%d",&n);
+printf("enter %d number:",n);
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+
+}
+for(i=1;i<n;i++){
+    for(j=i;j>=1;j--){
+        if(arr[j-1]<arr[j]){
+            temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+        }
+    }
+}
+printf("the sorted array is:");
+for(i=0;i<n;i++){
+    printf("%d ",arr[i]);
+}
+return 0;
+}
